952e8c7060025a0e0b65a89e19f987f32ab83a6a
mswelcome <mattwelcome@gmail.com>
2017-06-25 22:20:55 -0400

    This is the Homework for 26-Jun-2017

---
+++ rminds.rb
@@ -0,0 +1,20 @@
+#Counting to 100 While replacing certain values with Mind or MinedMinds. Homework for Monday 26-June-2017. Completed!!!
+1.upto(100) do |x|
+	if (x == 3)
+		puts "Mind"
+	elsif (x == 5)
+		puts "Mind"
+	elsif (x == 6)
+		puts "Mind"
+	elsif (x == 9)
+		puts "Mind"
+	elsif (x == 10)
+		puts "Mind"
+	elsif (x == 12)
+		puts "Mind"
+	elsif (x == 15)
+		puts "MinedMinds"
+	else
+		puts x
+	end
+end
