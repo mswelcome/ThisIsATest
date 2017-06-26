dea819768107ca5c1ea7b8367bf23b0b340c8cb6
mswelcome <mattwelcome@gmail.com>
2017-06-26 09:36:25 -0400

    The actual homework for 26-June-2017

---
+++ ex1.rb
@@ -0,0 +1,15 @@
+#Exercise 1
+1.upto(100) do |x|
+	if (x %15 == 0)
+		puts "MinedMinds"
+	elsif (x %5 == 0)
+		puts "Minds"
+	elsif (x %3 == 0)
+		puts "Mined"
+	elsif (x == 3)
+		puts "Mined"
+	elsif (x == 5)
+		puts "Minds"
+	else puts x
+	end
+end
